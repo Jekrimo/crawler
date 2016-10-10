100f56d4151afc369d302ae6ee50aabdb1ff5add
Jeffrey Morris <jmorris@us.ara.com>
2016-10-10 15:35:54 -0600

    crawler starts

---
+++ crawlerTest1.py
@@ -0,0 +1,48 @@
+# updates the copyright information for all .cs files
+# usage: call recursive_traversal, with the following parameters
+# parent directory, old copyright text content, new copyright text content
+
+import os
+
+excludedir = [""]
+
+def update_source(filename, oldcopyright, copyright):
+    utfstr = chr(0xef)+chr(0xbb)+chr(0xbf)
+    fdata = file(filename,"r+").read()
+    isUTF = False
+    if (fdata.startswith(utfstr)):
+        isUTF = True
+        fdata = fdata[3:]
+    if (oldcopyright != None):
+        if (fdata.startswith(oldcopyright)):
+            fdata = fdata[len(oldcopyright):]
+    if not (fdata.startswith(copyright)):
+        print "updating "+filename
+        fdata = copyright + fdata
+        if (isUTF):
+            file(filename,"w").write(utfstr+fdata)
+        else:
+            file(filename,"w").write(fdata)
+
+def recursive_traversal(dir,  oldcopyright, copyright):
+    global excludedir
+    fns = os.listdir(dir)
+    print "listing "+dir
+    for fn in fns:
+        fullfn = os.path.join(dir,fn)
+        if (fullfn in excludedir):
+            continue
+        if (os.path.isdir(fullfn)):
+            recursive_traversal(fullfn, oldcopyright, copyright)
+        else:
+            if (fullfn.endswith(".js") or fullfn.endswith(".css")):
+                update_source(fullfn, oldcopyright, copyright)
+            elif (fullfn.endswith(".html")):
+                update_source(fullfn, oldcopyright, file("copyrightTexthtml.txt","r+").read())    
+
+oldcright = file("oldcr.txt","r+").read()
+cright = file("copyrightTextcss.txt","r+").read()
+recursive_traversal(".", oldcright, cright)
+exit()
+
+
